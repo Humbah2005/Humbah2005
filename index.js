c728980bbd7448bb5450381e4a4b5c9c371e77e0
Humbah2005 <tanakapunungwe21@gmail.com>
2025-05-11 08:37:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const ownerNumber = ['263789677639']
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-const sessdata = config.SESSION_ID.replace("humbah-MD~", '');
+const sessdata = config.SESSION_ID.replace("humbah-md~", '');
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
